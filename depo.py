bd1788238655e0258c7835d3d3e1bcef258ba3f9
aginga <brianaginga35@gmail.com>
2022-10-27 10:07:42 +0300

    new lines

--- depo.py
+++ depo.py
@@ -15,6 +15,33 @@ symbol_count = {
     "C": 6,
     "D": 8
 }
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+
+    for line in range(lines):
+
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+
+            if symbol != symbol_to_check:
+         
+                break
+
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+    return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
 
@@ -102,8 +129,7 @@ def get_bet():
 
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     line = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -122,7 +148,27 @@ def main():
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print(slots)
 
+    winnings, winning_lines = check_winnings(slots, line, bet, symbol_value)
+    print(f'You won ${winnings}.')
+    print(f'you won on lines:', *winning_lines)
+
+    return winnings - total_bet
     
 
-main()
+def main():
+    balance = deposit()
+    while True:
+        print(f'current balance is ${balance}')
 
+        answer= input('press enter to play: (q to quit)')
+
+        if answer == 'q':
+            break
+
+        balance += spin(balance)
+
+
+    print(f'You left with ${balance}')
+
+
+main()
